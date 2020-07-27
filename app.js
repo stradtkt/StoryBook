df8c45bae0f1226ef3e371c4662860601d96765d
Kevin <stradtkt22@gmail.com>
2020-07-26 22:05:51 -0400

    Added the rest of the program

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const exphbs = require('express-handlebars');
+const methodOverride = require('method-override');
 const passport = require('passport');
 const session = require('express-session');
 const mongoose = require('mongoose');
@@ -17,12 +18,24 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+app.use(methodOverride(function(req, res) {
+    if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+        let method = req.body._method;
+        delete req.body._method;
+        return method;
+    }
+}));
+
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
-const { formatDate } = require('./helpers/hbs');
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs');
 app.engine('.hbs', exphbs({helpers: {
-    formatDate
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+    select
 }, defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine', '.hbs');
 //Sessions Middleware
@@ -37,6 +50,10 @@ app.use(session({
 //Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(function(req, res, next) {
+    res.locals.user = req.user || null;
+    next();
+})
 
 app.use(express.static(path.join(__dirname, 'public')));
 
